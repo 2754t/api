cd772b7e9352f0ce3bb225f3ac6ff2b82c9c9e24
舩越貴矢 <funakoshi@ad5.jp>
2024-01-22 08:52:19 +0900

    initial

---
+++ app/Http/Controllers/Player/FetchController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Player;
+
+use App\Http\Controllers\Controller;
+use App\Models\Player;
+
+class FetchController extends Controller
+{
+    public function __invoke()
+    {
+        dd(Player::all());
+        return Player::all();
+    }
+}
